1a13caff0c8e7bdcd41d3922f33b041ad240b066
KEVIN MORAN - Student <kmoran94@gmail.com>
2017-09-19 15:35:05 +0100

    Problem 2

---
+++ 02-time.go
@@ -0,0 +1,15 @@
+//Got From https://tour.golang.org/welcome/4
+//Current Time In Go
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,3 @@
 WOOOP||| U:\thirdYear\Data Rep Query\go-lab-c
+github mark down
+gonpp in plugin manger on notepad++ for syntax highlight not on college comp 
