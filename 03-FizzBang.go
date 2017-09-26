1cc627a076be75680110f38c6ace6f7de2a8466e
G00306474 <G00306474@gmit.ie>
2017-09-26 14:54:04 +0100

    Add files via upload

---
+++ 03-FizzBang.go
@@ -0,0 +1,26 @@
+//Got From: Own Code
+//Fizz Bang
+
+package main
+
+import (
+	"fmt"
+	
+)
+
+func main() {
+	 i = 0;
+	
+	for i=0;i<100;i++;
+	{
+		if i%3=0 & i%5=0 {
+		fmt.Println("Fizz Bang")
+		}
+		else if i%3 =0 {
+		fmt.Println(Fizz)}
+		else if i%5 =0 {
+		fmt.Println(Bang)}
+		else {
+		fmt.Println(i)}
+	}
+}
