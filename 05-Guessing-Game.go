cf5e77c09eba88ecd23b8c13dc0e1bddff923355
G00306474 <G00306474@gmit.ie>
2017-10-06 00:04:28 +0100

    code working except for previousGuess

--- 05-Guessing-Game.go
+++ 05-Guessing-Game.go
@@ -1,7 +1,8 @@
 //Kevin Moran 
 //G00306474@gmit.ie
 //Code Sourced:http://golangcookbook.blogspot.ie/2012/11/generate-random-number-in-given-range.html
-
+//Gave user option to enter high number cause if i give them the option to enter high and low program breaks
+//Also takes first guess without letting user enter input 
 package main
 import(
     "fmt"
@@ -9,13 +10,18 @@ import(
     "time"
 )
 
-func random(min, max int) int {
+//this generates random number between given range
+func xrand(min, max int) int {
     rand.Seed(time.Now().Unix())
     return rand.Intn(max - min) + min
 }
 
 func main() {
     
+    
+    guessTaken := 0
+    var guess int
+	var prevGuess int
 	var num1 = 1
 	var num2 = 0
 	
@@ -25,7 +31,34 @@ func main() {
 	fmt.Println("Please Enter High number:")//input number 
 	fmt.Scanf("%d", &num2)//scans number
 	
-	myrand := random(num1, num2)
-    fmt.Println(myrand)
-	
+	myrand := xrand(num1, num2)
+ 
+    fmt.Printf("I am thinking of a number between 1 and %d. You have 6 attempts \n",num2)
+    
+    //this is the while loop
+    for guessTaken < 6 {
+		fmt.Println("*********************")
+        fmt.Println("Take a guess...")
+        fmt.Scanf("%d\n",&guess)
+        guessTaken++
+		if guess == prevGuess {//does not work for some reason
+            guessTaken--
+			fmt.Println("Your guess is the same as last time.")
+        }
+		if guess < myrand {
+            fmt.Println("Your guess is too low.")
+        }else if guess > myrand {
+            fmt.Println("Your guess is too high.")
+        }else if guess == myrand {
+            break
+        }
+		
+		guess = prevGuess
+		fmt.Printf("Count %d \n", guessTaken)
+    }
+    if guess == myrand {
+        fmt.Printf("Good job! You guessed my number in %d tries\n", guessTaken)
+    } else {
+        fmt.Printf("Nope. The number I had in mind was %d\n", myrand)
+    }
 }
