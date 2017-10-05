cb693e01f19d501bd30fcfd3c2db0fc15592e3d0
G00306474 <G00306474@gmit.ie>
2017-10-05 14:08:50 +0100

    FizzBang code running with comments added

--- 03-FizzBang.go
+++ 03-FizzBang.go
@@ -3,24 +3,17 @@
 
 package main
 
-import (
-	"fmt"
-	
-)
+import "fmt"
 
-func main() {
-	 i = 0;
-	
-	for i=0;i<100;i++;
-	{
-		if i%3=0 & i%5=0 {
-		fmt.Println("Fizz Bang")
-		}
-		else if i%3 =0 {
-		fmt.Println(Fizz)}
-		else if i%5 =0 {
-		fmt.Println(Bang)}
-		else {
-		fmt.Println(i)}
-	}
-}
+func main() {//Start Main
+	for i := 1;i<=100; i++{//Start for Loop, { to be on this line or wont run
+		if i%3==0 && i%5==0 {//if i divided by three equal 0 and i divided by five equal zero go in here
+		fmt.Println("Fizz Bang")//print fizz bang to screen 
+		} else if i%3 ==0 {//if i divided by three equal 0 go in here
+		fmt.Println("Fizz")//print fizz to screen 
+		} else if i%5 ==0 {//if i divided by five equal 0 go in here
+		fmt.Println("Bang")//print bang to screen 
+		}else {//if no condition is met go in here
+		fmt.Println(i)}//print number to screen
+	}//End for Loop
+}//End Main
