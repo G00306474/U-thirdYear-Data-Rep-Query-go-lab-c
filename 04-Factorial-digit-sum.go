198c8d14e51dc4d8abd6f01ed737385231e0941f
G00306474 <G00306474@gmit.ie>
2017-10-06 23:57:05 +0100

    still not fully functional, commented

--- 04-Factorial-digit-sum.go
+++ 04-Factorial-digit-sum.go
@@ -19,18 +19,20 @@ func main() {
 	fmt.Scanf("%d", &num)//scans number
 
 
-	if num < 0{
+	if num <= 0{
 		fmt.Println("Number cannot be less than 0!")
-		for num < 0{
+		for num <= 0{
 			fmt.Println("Please Enter a number greater than zero:")//input number 
 			fmt.Scanf("%d", &num)//scans number
+			fmt.Scan("%d", &num)
 		}
-	}else{
+	}
+	
 
-		for i := 1; i <= num; i++{
-			factorial = factorial * i//makes the factorial equal too the factorial * i 
-			total = total + i
-		}
+	for i := 1; i <= num; i++{
+		factorial = factorial * i//makes the factorial equal too the factorial * i 
+		total = total + i
+	
 	}
 	for i := 1; i <= num; i++{
 			factorial = factorial * i//makes the factorial equal too the factorial * i 
