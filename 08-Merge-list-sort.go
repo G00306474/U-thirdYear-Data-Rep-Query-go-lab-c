f5b6df2ef0ccf5b5cd35a12f30b4e4b90b4b4439
G00306474 <G00306474@gmit.ie>
2017-10-06 01:12:09 +0100

    code commented

--- 08-Merge-list-sort.go
+++ 08-Merge-list-sort.go
@@ -11,19 +11,18 @@ import (
 
 
 func main(){
-		//array1 := []int{10, 40, 60}
-		//array2 := []int{20, 30, 50}
+		
 	var n int
 	var r int
 	
 	fmt.Println("Please Enter size of array 1:")
   fmt.Scan(&n)
  
-  array1 := make([]int, n)
+  array1 := make([]int, n)//creates array to user sze 
   for i := 0; i < n; i++ {
-      fmt.Scan(&array1[i])
+      fmt.Scan(&array1[i])//takes input for each postition
     }
-  fmt.Println("Nums in array 1",array1)
+  fmt.Println("Nums in array 1",array1)//prints contents of array
 	
 	fmt.Println("Please Enter size of array 2:")
   fmt.Scan(&r)
@@ -38,8 +37,8 @@ func main(){
 	
 	
 	
-		mergedArray := append(array1, array2...)
+		mergedArray := append(array1, array2...)//merges array1 and array2
 	
-		sort.Ints(mergedArray)//sorts array by size
-		fmt.Println("List AB Merged and Sorted: ",mergedArray)
+		sort.Ints(mergedArray)//sorts merged array by size
+		fmt.Println("List Array Merged and Sorted: ",mergedArray)//prints merged array
 }//main
